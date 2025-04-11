2395b691cc8eab3b32c8cb69ad8018fed244f58b
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2025-04-11 16:47:13 -0700

    more details in repro

--- tst_tests.cpp
+++ tst_tests.cpp
@@ -2,14 +2,20 @@
 
 using namespace testing;
 
-TEST(Tests, TestRepro)
+TEST(Tests, TestReproCout)
 {
-    std::cout << "This is a message";
+    std::cout << "This is a message not ending in newline";
     EXPECT_EQ(1, 1);
 }
 
-TEST(Tests, TestNoRepro)
+TEST(Tests, TestNoReproCoutNewline)
 {
-    std::cout << "This is a message\n";
+    std::cout << "This is a message ending in newline\n";
+    EXPECT_EQ(1, 1);
+}
+
+TEST(Tests, TestNoReproCerr)
+{
+    std::cerr << "This is a message not ending in newline to cerr";
     EXPECT_EQ(1, 1);
 }
